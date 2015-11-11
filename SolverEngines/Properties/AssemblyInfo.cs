f73c097b9d01b57941221e08df41a99d20f406a6
blowfishpro <blowfishpro@yahoo.com>
2015-11-10 21:07:05 -0800

    One more ModuleAnimateEmissive reference

--- SolverEngines/Properties/AssemblyInfo.cs
+++ SolverEngines/Properties/AssemblyInfo.cs
@@ -35,5 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: KSPAssemblyDependency("ModuleAnimateEmissive", 1, 0)]
 [assembly: KSPAssembly("SolverEngines", 1, 0)]
