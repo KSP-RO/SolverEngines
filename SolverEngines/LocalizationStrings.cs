bd97885d2c8f13edc6afcfe6942d542d34e08dcf
Tinygrox <tinygrox@gmail.com>
2023-11-11 21:09:26 +0800

    Update LocalizationStrings.cs

--- SolverEngines/LocalizationStrings.cs
+++ SolverEngines/LocalizationStrings.cs
@@ -8,130 +8,130 @@ namespace SolverEngines
         /// <summary>
         /// "Engines GUI"
         /// </summary>
-        public static string str_EnginesGUI = Localizer.GetStringByTag("#SolverEngines_EnginesGUI");
+        public static string str_EnginesGUI = Localizer.Format("#SolverEngines_EnginesGUI");
         /// <summary>
         /// "Ambient Temperature"
         /// </summary>
-        public static string str_AmbientTemperature = Localizer.GetStringByTag("#SolverEngines_AmbientTemperature");
+        public static string str_AmbientTemperature = Localizer.Format("#SolverEngines_AmbientTemperature");
         /// <summary>
         /// "Ambient Pressure"
         /// </summary>
-        public static string str_AmbientPressure = Localizer.GetStringByTag("#SolverEngines_AmbientPressure");
+        public static string str_AmbientPressure = Localizer.Format("#SolverEngines_AmbientPressure");
         /// <summary>
         /// "Recovery Temperature"
         /// </summary>
-        public static string str_RecoveryTemperature = Localizer.GetStringByTag("#SolverEngines_RecoveryTemperature");
+        public static string str_RecoveryTemperature = Localizer.Format("#SolverEngines_RecoveryTemperature");
         /// <summary>
         /// "Recovery Pressure"
         /// </summary>
-        public static string str_RecoveryPressure = Localizer.GetStringByTag("#SolverEngines_RecoveryPressure");
+        public static string str_RecoveryPressure = Localizer.Format("#SolverEngines_RecoveryPressure");
         /// <summary>
         /// "Inlet"
         /// </summary>
-        public static string str_Inlet = Localizer.GetStringByTag("#SolverEngines_Inlet");
+        public static string str_Inlet = Localizer.Format("#SolverEngines_Inlet");
         /// <summary>
         /// "TPR"
         /// </summary>
-        public static string str_TPR = Localizer.GetStringByTag("#SolverEngines_TPR");
+        public static string str_TPR = Localizer.Format("#SolverEngines_TPR");
         /// <summary>
         /// "Inlet Pressure Ratio"
         /// </summary>
-        public static string str_InletPressureRatio = Localizer.GetStringByTag("#SolverEngines_InletPressureRatio");
+        public static string str_InletPressureRatio = Localizer.Format("#SolverEngines_InletPressureRatio");
         /// <summary>
         /// "Thrust"
         /// </summary>
-        public static string str_Thrust = Localizer.GetStringByTag("#SolverEngines_Thrust");
+        public static string str_Thrust = Localizer.Format("#SolverEngines_Thrust");
         /// <summary>
         /// "TWR"
         /// </summary>
-        public static string str_TWR = Localizer.GetStringByTag("#SolverEngines_TWR");
+        public static string str_TWR = Localizer.Format("#SolverEngines_TWR");
         /// <summary>
         /// "Thrust / Drag"
         /// </summary>
-        public static string str_ThrustDrag = Localizer.GetStringByTag("#SolverEngines_ThrustDrag");
+        public static string str_ThrustDrag = Localizer.Format("#SolverEngines_ThrustDrag");
         /// <summary>
         /// "Specific Impulse"
         /// </summary>
-        public static string str_SpecificImpulse = Localizer.GetStringByTag("#SolverEngines_SpecificImpulse");
+        public static string str_SpecificImpulse = Localizer.Format("#SolverEngines_SpecificImpulse");
         /// <summary>
         /// "TSFC"
         /// </summary>
-        public static string str_TSFC = Localizer.GetStringByTag("#SolverEngines_TSFC");
+        public static string str_TSFC = Localizer.Format("#SolverEngines_TSFC");
         /// <summary>
         /// "Settings"
         /// </summary>
-        public static string str_Settings = Localizer.GetStringByTag("#SolverEngines_Settings");
+        public static string str_Settings = Localizer.Format("#SolverEngines_Settings");
         /// <summary>
         /// "Show Ambient Temperature"
         /// </summary>
-        public static string str_ShowAmbientTemperature = Localizer.GetStringByTag("#SolverEngines_ShowAmbientTemperature");
+        public static string str_ShowAmbientTemperature = Localizer.Format("#SolverEngines_ShowAmbientTemperature");
         /// <summary>
         /// "Show Ambient Pressure"
         /// </summary>
-        public static string str_ShowAmbientPressure = Localizer.GetStringByTag("#SolverEngines_ShowAmbientPressure");
+        public static string str_ShowAmbientPressure = Localizer.Format("#SolverEngines_ShowAmbientPressure");
         /// <summary>
         /// "Show Inlet Percentage"
         /// </summary>
-        public static string str_ShowInletPercentage = Localizer.GetStringByTag("#SolverEngines_ShowInletPercentage");
+        public static string str_ShowInletPercentage = Localizer.Format("#SolverEngines_ShowInletPercentage");
         /// <summary>
         /// "Show Recovery Temperature"
         /// </summary>
-        public static string str_ShowRecoveryTemperature = Localizer.GetStringByTag("#SolverEngines_ShowRecoveryTemperature");
+        public static string str_ShowRecoveryTemperature = Localizer.Format("#SolverEngines_ShowRecoveryTemperature");
         /// <summary>
         /// "Show Recovery Pressure"
         /// </summary>
-        public static string str_ShowRecoveryPressure = Localizer.GetStringByTag("#SolverEngines_ShowRecoveryPressure");
+        public static string str_ShowRecoveryPressure = Localizer.Format("#SolverEngines_ShowRecoveryPressure");
         /// <summary>
         /// "Show Total Pressure Recovery"
         /// </summary>
-        public static string str_ShowTotalPressureRecovery = Localizer.GetStringByTag("#SolverEngines_ShowTotalPressureRecovery");
+        public static string str_ShowTotalPressureRecovery = Localizer.Format("#SolverEngines_ShowTotalPressureRecovery");
         /// <summary>
         /// "Show Thrust"
         /// </summary>
-        public static string str_ShowThrust = Localizer.GetStringByTag("#SolverEngines_ShowThrust");
+        public static string str_ShowThrust = Localizer.Format("#SolverEngines_ShowThrust");
         /// <summary>
         /// "Show Thrust to Weight Ratio"
         /// </summary>
-        public static string str_ShowThrusttoWeightRatio = Localizer.GetStringByTag("#SolverEngines_ShowThrusttoWeightRatio");
+        public static string str_ShowThrusttoWeightRatio = Localizer.Format("#SolverEngines_ShowThrusttoWeightRatio");
         /// <summary>
         /// "Show Thrust / Drag"
         /// </summary>
-        public static string str_ShowThrustDrag = Localizer.GetStringByTag("#SolverEngines_ShowThrustDrag");
+        public static string str_ShowThrustDrag = Localizer.Format("#SolverEngines_ShowThrustDrag");
         /// <summary>
         /// "Show Isp"
         /// </summary>
-        public static string str_ShowIsp = Localizer.GetStringByTag("#SolverEngines_ShowIsp");
+        public static string str_ShowIsp = Localizer.Format("#SolverEngines_ShowIsp");
         /// <summary>
         /// "Show TSFC"
         /// </summary>
-        public static string str_ShowTSFC = Localizer.GetStringByTag("#SolverEngines_ShowTSFC");
+        public static string str_ShowTSFC = Localizer.Format("#SolverEngines_ShowTSFC");
         /// <summary>
         /// "Engines Flight GUI Settings"
         /// </summary>
-        public static string str_EnginesFlightGUISettings = Localizer.GetStringByTag("#SolverEngines_EnginesFlightGUISettings");
+        public static string str_EnginesFlightGUISettings = Localizer.Format("#SolverEngines_EnginesFlightGUISettings");
         /// <summary>
         /// "Engines GUI Units Settings"
         /// </summary>
-        public static string str_EnginesGUIUnitsSettings = Localizer.GetStringByTag("#SolverEngines_EnginesGUIUnitsSettings");
+        public static string str_EnginesGUIUnitsSettings = Localizer.Format("#SolverEngines_EnginesGUIUnitsSettings");
         /// <summary>
         /// "Units"
         /// </summary>
-        public static string str_Units = Localizer.GetStringByTag("#SolverEngines_Units");
+        public static string str_Units = Localizer.Format("#SolverEngines_Units");
         /// <summary>
         /// "AJE Inlet"
         /// </summary>
-        public static string str_AJEInlet = Localizer.GetStringByTag("#SolverEngines_AJEInlet");
+        public static string str_AJEInlet = Localizer.Format("#SolverEngines_AJEInlet");
         /// <summary>
         /// "Intake Area"
         /// </summary>
-        public static string str_IntakeArea = Localizer.GetStringByTag("#SolverEngines_IntakeArea");
+        public static string str_IntakeArea = Localizer.Format("#SolverEngines_IntakeArea");
         /// <summary>
         /// "Intake Resource"
         /// </summary>
-        public static string str_IntakeResource = Localizer.GetStringByTag("#SolverEngines_IntakeResource");
+        public static string str_IntakeResource = Localizer.Format("#SolverEngines_IntakeResource");
         /// <summary>
         /// "Eng. Int."
         /// </summary>
-        public static string str_EIT = Localizer.GetStringByTag("#SolverEngines_EIT");
+        public static string str_EIT = Localizer.Format("#SolverEngines_EIT");
     }
 }
